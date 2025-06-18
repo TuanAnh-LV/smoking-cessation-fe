99ee9d234b6ff37762e0cbdd82310294de2d5df5
Tran Hai Son <sonthse172913@fpt.edu.vn>
2025-06-18 10:38:04 +0700

    create payment

--- src/pages/PaymentPage/PaymentPage.jsx
+++ src/pages/PaymentPage/PaymentPage.jsx
@@ -1,16 +1,46 @@
 import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import "./PaymentPage.scss";
 import paypal from "../../assets/paypal.png";
 import momo from "../../assets/momo.png";
 
+import { PaymentService } from "../../services/payment.service";
+
 const PaymentPage = () => {
   const [paymentMethod, setPaymentMethod] = useState("momo");
+  const location = useLocation();
+  const packageInfo = location.state || {
+    title: "Membership Package",
+    price: 0,
+    desc: "",
+    date: ""
+  };
+
+  const handleCreatePaypalOrder = async () => {
+    try {
+      const res = await PaymentService.createPaypalOrder({
+        package_id: packageInfo._id
+      });
+      console.log("Send package_id:", packageInfo._id);
+      console.log("Paypal order created:", res);
+
+      if (res.data && res.data.approveUrl) {
+        window.location.href = res.data.approveUrl;  // Redirect to PayPal
+      } else {
+        console.error("No approveUrl received");
+      }
+    } catch (error) {
+      console.error("Create Paypal Order failed:", error);
+    }
+  };
 
   return (
+
     <div className="payment-page">
       <div className="payment-container">
+
         {/* Payment Information */}
-        <div className="payment-info-card">
+        {/* <div className="payment-info-card">
           <h2 className="section-title">Payment information</h2>
           <form className="payment-form">
             <div className="form-group">
@@ -36,16 +66,14 @@ const PaymentPage = () => {
               <input type="email" id="email" placeholder="abc@gmail.com" />
             </div>
           </form>
-        </div>
+        </div> */}
 
-        {/* Payment Method */}
         <div className="payment-method-card">
           <h2 className="section-title">Payment method</h2>
           <div className="payment-method-options">
             <label
-              className={`method-option ${
-                paymentMethod === "card" ? "selected" : ""
-              }`}
+              className={`method-option ${paymentMethod === "card" ? "selected" : ""
+                }`}
             >
               <input
                 type="radio"
@@ -56,13 +84,12 @@ const PaymentPage = () => {
               />
               Pay-pal
               <span className="card-icons">
-                <img src={paypal} alt="Visa" />
+                <img src={paypal} alt="Paypal" />
               </span>
             </label>
             <label
-              className={`method-option ${
-                paymentMethod === "momo" ? "selected" : ""
-              }`}
+              className={`method-option ${paymentMethod === "momo" ? "selected" : ""
+                }`}
             >
               <input
                 type="radio"
@@ -82,16 +109,17 @@ const PaymentPage = () => {
           </div>
         </div>
 
-        {/* Order Details */}
         <div className="order-details-card">
           <h2 className="section-title">Order details</h2>
           <div className="membership-package">
             <div className="package-info">
-              <span className="package-title">Membership Package</span>
-              <span className="package-price">499.000đ</span>
+              <span className="package-title">{packageInfo.title.toUpperCase()}</span>
+              <span className="package-price">
+                {packageInfo.price.toLocaleString()} VND
+              </span>
             </div>
-            <div className="package-desc">1 month</div>
-            <div className="package-date">17/06/2025 - 17/06/2025</div>
+            <div className="package-desc">{packageInfo.desc}</div>
+            <div className="package-date">{packageInfo.date}</div>
           </div>
           <div className="form-group">
             <label htmlFor="discountCode">Discount code</label>
@@ -99,9 +127,11 @@ const PaymentPage = () => {
           </div>
           <div className="order-total-row">
             <span>Total</span>
-            <span className="order-total">499.000 VND</span>
+            <span className="order-total">
+              {packageInfo.price.toLocaleString()} VND
+            </span>
           </div>
-          <button className="confirm-payment-btn">
+          <button className="confirm-payment-btn" onClick={handleCreatePaypalOrder}>
             Confirmation and Payment
           </button>
           <div className="order-note">
